c6a7d2a9972429a0eed8a7f474f56dc1eaff4287
Cami4444 <165091338+Cami4444@users.noreply.github.com>
2025-09-18 17:09:20 -0500

    Add files via upload

---
+++ REPETICION.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    
+    for (int i=1; i<8; i++){
+        printf("\n");
+        for (int j=1; i<=18; j++)
+            printf("%d\t", j*i);
+}
+}
